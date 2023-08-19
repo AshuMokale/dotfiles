13bb42a8dba785e3bb72db69231b296a73097ff6
Ashish Mokale <62004342+AshuMokale@users.noreply.github.com>
2023-08-19 17:31:49 +0530

    Started neovim config.

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,1 @@
+require('satoru.options')
---
+++ nvim/.config/nvim/lua/satoru/options.lua
@@ -0,0 +1,19 @@
+-- Shortend API
+local opt = vim.opt
+
+-- Options
+opt.relativenumber = true
+opt.numberwidth = 2
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.smartindent = true
+opt.autoindent = true
+opt.cursorline = true
+opt.hlsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.backup = false
+opt.splitbelow = true
+opt.splitright = true
+opt.termguicolors = true
