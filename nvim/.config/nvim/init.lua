ff92171f39ed3f958fbd4936118d55fac9c0eda9
Ashish Mokale <62004342+AshuMokale@users.noreply.github.com>
2023-08-23 19:25:16 +0530

    Trying to add plugins.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,4 @@
 require('satoru.options')
 require('satoru.keymaps')
 require('satoru.lazy-nvim')
+require('satoru.plugins')
---
+++ nvim/.config/nvim/lua/satoru/plugins.lua
@@ -0,0 +1,3 @@
+require('lazy').setup({
+  { 'rose-pine/neovim', name = 'rose-pine' },
+})
