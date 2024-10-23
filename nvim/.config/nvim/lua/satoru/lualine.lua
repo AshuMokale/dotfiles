fcf02a4b9214d62ab3f4661ca07253de1f1e5642
AshuMokale <ashishmokale2002@gmail.com>
2024-10-23 23:06:04 +0530

    added bufferline to lualine(neovim)

--- nvim/.config/nvim/lua/satoru/lualine.lua
+++ nvim/.config/nvim/lua/satoru/lualine.lua
@@ -1,5 +1,18 @@
 require('lualine').setup {
 	options = {
 		theme = 'gruvbox',
+		section_separators = { left = '', right = '' },
+		component_separators = { left = '', right = '' }
 	},
+	sections = {
+		lualine_x = { "filetype" }
+	},
+	tabline = {
+		lualine_a = {'buffers'},
+		-- lualine_b = {'branch'},
+		-- lualine_c = {'filename'},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = {'tabs'}
+	}
 }
