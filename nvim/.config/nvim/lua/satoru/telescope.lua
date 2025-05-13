7d107b502197651a4a0f6bd74d3f5ef637334775
AshuMokale <ashishmokale2002@gmail.com>
2025-05-13 11:18:47 +0530

    Removed unnecessary and old configs.

--- nvim/.config/nvim/lua/satoru/telescope.lua
+++
@@ -1,7 +0,0 @@
-require('telescope').setup {}
-
--- Telescope keys
-local set = vim.keymap.set
-local builtin = require('telescope.builtin')
-set('n', '<C-p>', builtin.find_files) -- find files
-set('n', '<leader>fh', builtin.help_tags) -- Help tags
