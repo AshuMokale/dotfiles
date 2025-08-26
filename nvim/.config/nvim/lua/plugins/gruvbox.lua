e16079777d5b16031e20be6e58c01aed219853d0
Ashish Mokale <62004342+AshuMokale@users.noreply.github.com>
2025-08-26 15:28:34 +0530

    Renabled cmd completion disabled by NvimCmp and added rose-pine theme.

--- nvim/.config/nvim/lua/plugins/gruvbox.lua
+++ nvim/.config/nvim/lua/plugins/gruvbox.lua
@@ -2,9 +2,9 @@ return {
 	"sainnhe/gruvbox-material",
 	lazy = false,
 	priority = 1000,
-	config = function()
-		vim.g.gruvbox_material_background = "medium"
-		vim.g.gruvbox_material_better_performance = 1
-		vim.cmd("colorscheme gruvbox-material")
-	end
+	-- config = function()
+	-- 	vim.g.gruvbox_material_background = "medium"
+	-- 	vim.g.gruvbox_material_better_performance = 1
+	-- 	vim.cmd("colorscheme gruvbox-material")
+	-- end
 }
