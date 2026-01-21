370fbe9628297eb68a79274ccf05738c05e83ebf
Ashish Mokale <ashishmokale2002@gmail.com>
2026-01-21 15:10:43 +0530

    changed telescope preview window's settings

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,15 +1,19 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	-- or                              , branch = '0.1.x',
-	dependencies = { "nvim-lua/plenary.nvim" },
+	tag = "v0.2.1",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		-- optional but recommended
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
 	opts = {
 		defaults = {
 			-- View telescope.layout docs
-			-- layout_strategy = "vertical",
-			-- layout_config = {
-			-- 	preview_cutoff = 0.4,
-			-- },
+			layout_strategy = "horizontal",
+			layout_config = {
+				preview_cutoff = 0.4,
+				prompt_position = "top",
+			},
 			file_ignore_patterns = {
 				"%.class",
 				"^bin/",
